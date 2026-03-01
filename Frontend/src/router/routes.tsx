607d014e86471c955c0c57029acb497b2fae6c07
Marcin Kowalski <marcinmac@MacBook-Pro-Marcin.local>
2026-03-01 16:25:56 +0100

    small fix

--- Frontend/src/router/routes.tsx
+++ Frontend/src/router/routes.tsx
@@ -1,6 +1,6 @@
 import type RouteConfig from "./routeInteface.ts";
 import landingPageRoute from "../modules/RoutedModules/LandingPageModule/module.export.tsx";
-import userInfoRoute from "../modules/userInfoModule/module.export.tsx";
+import userInfoRoute from "../modules/RoutedModules/userInfoModule/module.export.tsx";
 import DiceRollerRoute from "../modules/DiceRollerModule/module.export.tsx";
 
 const routes: RouteConfig[] = [
